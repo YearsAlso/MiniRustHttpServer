182fb536878e7a0234d0ee775c6a52050653f00e
YearsAlso <1334713380@qq.com>
2025-10-07 20:15:58 +0800

    feat: add 404 resp

--- src/main.rs
+++ src/main.rs
@@ -15,18 +15,16 @@ fn main() {
 
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
-    let http_request: Vec<_> = buf_reader
-        .lines()
-        .map(|result| result.unwrap())
-        .take_while(|line| !line.is_empty())
-        .collect();
+    let request_lines = buf_reader.lines().next().unwrap().unwrap();
 
-    println!("Request: {:?}", http_request);
+    let (status_line, filename) = if request_lines == "GET / HTTP/1.1" {
+        ("HTTP/1.1 200 OK", "index.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    };
 
-    let status_line = "HTTP/1.1 200 OK";
-    let content = fs::read_to_string("hello.html").unwrap();
+    let content = fs::read_to_string(filename).unwrap();
     let length = content.len();
-
     let response = format!(
         "{status_line}\r\n \
         Content-Length:{length} \r\n\r\n\
